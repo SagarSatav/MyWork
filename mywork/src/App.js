dedcb8894df9b738e7243215d5471459628bba05
SagarSatav <satavsagar17@gmail.com>
2024-04-21 12:48:55 +0530

    inial commit

---
+++ mywork/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   hello my name is sagar
+    </div>
+  );
+}
+
+export default App;
